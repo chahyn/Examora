2f2367fff87efbcabfce743ae47eb684850d57ef
test <hinecha886@gmail.com>
2026-03-28 12:11:40 +0100

    main struture

---
+++ src/main/java/org/examora/examora/ExamoraApplication.java
@@ -0,0 +1,13 @@
+package org.examora.examora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamoraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/examora/examora/ExamoraApplicationTests.java
@@ -0,0 +1,13 @@
+package org.examora.examora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamoraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
